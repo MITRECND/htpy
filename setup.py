422000a1ce13924105b6efabb85f5d3f69a451ff
Wesley Shields <wxs@atarininja.org>
2014-03-24 11:19:25 -0400

    Update to 0.17.
    This uses libhtp-0.5.10.

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ import os, os.path
 
 pathjoin = os.path.join
 
-GITVER   = '0.5.9'
-PKGNAME  = 'libhtp-' + GITVER
+GITVER   = '0.5.10'
+PKGNAME  = 'htp-' + GITVER
 PKGTAR   = PKGNAME + '.tar.gz'
-BUILDDIR = 'libhtp-' + GITVER
+BUILDDIR = 'htp-' + GITVER
 
 INCLUDE_DIRS  = ['/usr/local/include', '/opt/local/include', '/usr/include']
 LIBRARY_DIRS  = ['/usr/lib', '/usr/local/lib']
@@ -37,7 +37,7 @@ class htpyMaker(build):
 
         spawn(['tar', '-zxf', self.HTPTAR], search_path = 1)
         os.chdir(self.HTPDIR)
-        spawn([pathjoin('.','autogen.sh')], '-i')
+        #spawn([pathjoin('.','autogen.sh')], '-i')
         spawn([pathjoin('.','configure'), 'CFLAGS=-fPIC'])
         spawn(['make'], search_path = 1)
         os.chdir('..')
@@ -51,7 +51,7 @@ EXTRA_OBJECTS = htpyMaker.extra_objects + EXTRA_OBJECTS
 
 setup (# Distribution meta-data
         name = "htpy",
-        version = "0.16",
+        version = "0.17",
         description = "python bindings for libhtp",
         author = "Wesley Shields",
         author_email = "wshields@mitre.org",
